9c8bfdd38a4d1bc3e36dd8c3a7404c6cf1fe9495
iieunji <iieunji023@gmail.com>
2024-11-18 10:56:13 +0900

    Feat: 기본 문법과 쿼리 API - 실습 엔티티 생성 - JPQL 문법 - TypeQuery, Query - 결과 조회 API - 파라미터 바인딩

---
+++ src/main/java/com/hellojpql/jpql/JpaMain.java
@@ -0,0 +1,42 @@
+package com.hellojpql.jpql;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+
+            System.out.println("singleResult = " + result.getUsername());
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();     // 데이터베이스 커넥션 반환
+        }
+        emf.close();
+    }
+
+}
