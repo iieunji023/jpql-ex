be67cf7cbdcbe2a9e43ab2f1e191f28fe81afed3
iieunji <iieunji023@gmail.com>
2024-11-20 18:01:02 +0900

    Docs: 다형성 쿼리, 엔티티 직접 사용 리드미 정리

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -42,16 +42,10 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t From Team t";
-            List<Team> result = em.createQuery(query, Team.class).setFirstResult(0).setMaxResults(2).getResultList();
+            String query = "select m From Member m where m.team = :team";
+            Member findMember = em.createQuery(query, Member.class).setParameter("team", teamB).getSingleResult();
 
-            for (Team team : result) {
-                System.out.println("team= " + team.getName() + " |members= " + team.getMembers().size());
-                for(Member member : team.getMembers()) {
-                    System.out.println("-> member = " + member);
-                }
-
-            }
+            System.out.println("findMember = " + findMember);
 
             tx.commit();
         } catch (Exception e){
